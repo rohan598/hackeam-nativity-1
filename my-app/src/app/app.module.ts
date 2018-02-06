1ed2f19c91effab67e57d3cd219b714acecedb62
rohan598 <rohanwadhawan7@gmail.com>
2018-02-06 23:04:09 +0530

    added society login

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,7 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { LoginUserComponent } from './login/login-user/login-user.component';
 import { LoginSocietyComponent } from './login/login-society/login-society.component';
 import { CreateFormComponent } from './create-form/create-form.component';
-
+import {} from '@types/googlemaps';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,13 @@ import { CreateFormComponent } from './create-form/create-form.component';
     CreateFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey:'YOUR_API_KEY',
+      libraries:["places"]
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/login/login-user/login-user.component.ts
+++ my-app/src/app/login/login-user/login-user.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+// import { } from 'googlemaps';
+// import { MapsAPILoader } from '@agm/core';
+
 
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
   styleUrls: ['./login-user.component.css']
 })
+
 export class LoginUserComponent implements OnInit {
+  constructor(){}
+  ngOnInit(){
 
-  constructor() { }
-
-  ngOnInit() {
   }
-
 }
